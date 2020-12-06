daf3119c4e043a7a0ee2b977555959eda86e0c6e
Jake Meyers <jakejake92@yahoo.com>
2020-12-05 23:19:08 -0700

    sent response

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.use(() => {
+app.use((req, res) => {
 	console.log("we got a request");
+	res.send("hello, we got your request");
 });
 
 app.listen(3000, () => {
